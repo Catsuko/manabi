f004f608feff4d2bf809ccf374ea9ad2b546d22c
Lewis Reid <lewis@nanameue.jp>
2022-05-03 21:31:18 +0900

    refactored out duplicate route definitions using sinatra namespacing

--- lib/api.rb
+++ lib/api.rb
@@ -11,27 +11,27 @@ set :default_content_type, :json
 set :show_exceptions, :after_handler
 
 # TODO: Remove Duplication in parameter parsing
-# TODO: Remove Duplication in route definitions
 # TODO: Parameter Validation (required, in bounds etc)
-# TODO: Refactor to modular api so each resource can be in a separate file
 
-get '/decks/:id/topics' do |deck_id|
-  cache_control :no_store
+namespace '/decks/:id/topics' do
+  get do |deck_id|
+    cache_control :no_store
 
-  deck = Manabi.create_deck(deck_id)
-  deck.peek(params[:number].to_i).to_json
-end
+    deck = Manabi.create_deck(deck_id)
+    deck.peek(params[:number].to_i).to_json
+  end
 
-post '/decks/:id/topics' do |deck_id|
-  deck = Manabi.create_deck(deck_id)
-  topics = Manabi.create_topics(*params.fetch(:topics, []))
-  deck.add(*topics).to_json
-end
+  post do |deck_id|
+    deck = Manabi.create_deck(deck_id)
+    topics = Manabi.create_topics(*params.fetch(:topics, []))
+    deck.add(*topics).to_json
+  end
 
-delete '/decks/:id/topics' do |deck_id|
-  deck = Manabi.create_deck(deck_id)
-  topics = Manabi.create_topics(*params.fetch(:topics, []))
-  deck.take(*topics).to_json
+  delete do |deck_id|
+    deck = Manabi.create_deck(deck_id)
+    topics = Manabi.create_topics(*params.fetch(:topics, []))
+    deck.take(*topics).to_json
+  end
 end
 
 error ArgumentError do
